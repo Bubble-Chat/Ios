51050b9b8db3a19ded1066476e1120b98b4b61c9
Jo-root <whqkrqkr12345@gmail.com>
2023-05-24 16:54:41 +0900

    Initial Commit

---
+++ BubbleChat/BubbleChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleChatApp.swift
+//  BubbleChat
+//
+//  Created by 조근호 on 2023/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BubbleChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
