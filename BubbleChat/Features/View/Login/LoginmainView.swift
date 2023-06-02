b18f5e66d840511e7ff6797f8561c931fbb9a901
Jo-root <whqkrqkr12345@gmail.com>
2023-06-02 22:50:25 +0900

    Created  LoginmainView

---
+++ BubbleChat/Features/View/Login/LoginmainView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginmainView.swift
+//  BubbleChat
+//
+//  Created by 조근호 on 2023/06/02.
+//
+
+import SwiftUI
+
+struct LoginmainView: View {
+    var body: some View {
+        Text("a")
+    }
+}
+
+struct LoginmainView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
